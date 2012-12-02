5ef6efcd71a47a54802d9eb687b8d79f39070cff
Josh Blum <josh@joshknows.com>
2012-12-01 17:25:24 -0800

    cleanup and use single allocation

--- include/PMC/Detail/PMCImpl.hpp
+++ include/PMC/Detail/PMCImpl.hpp
@@ -18,15 +18,15 @@ struct PMCImpl
     PMCImpl(void):
         count(0),
         item(NULL),
-        buff(buff_fixed)
+        buff(NULL)
     {
         //NOP
     }
 
-    ~PMCImpl(void)
+    void reset(void)
     {
         if (item) item->reset();
-        if (buff != buff_fixed) delete [] buff;
+        if (buff) delete [] buff;
     }
 
     boost::detail::atomic_count count;
@@ -78,15 +78,7 @@ struct PMCImpl
         return static_cast<Container<CastType> *>(item)->value;
     }
 
-    //! The fixed size storage buffer
-    char buff_fixed[PMC_FIXED_BUFF_SIZE];
     char *buff;
-
-    void *alloc(const size_t bytes)
-    {
-        if (bytes > PMC_FIXED_BUFF_SIZE) buff = new char[bytes];
-        return buff;
-    }
 };
 
 /***********************************************************************
@@ -106,7 +98,7 @@ PMC_INLINE void intrusive_ptr_release(PMCImpl *impl)
 {
     if (--impl->count == 0)
     {
-        delete impl;
+        impl->reset();
     }
 }
 
@@ -171,10 +163,18 @@ PMC_INLINE ValueType &PMC::as(void) const
 template <typename ValueType>
 PMC_INLINE PMC PMC_M(const ValueType &value)
 {
-    PMC p;
-    p.reset(new PMCImpl());
-    void *buff = p->alloc(sizeof(ValueType));
-    p->item = new (buff) PMCImpl::Container<ValueType>(value);
+    //create a buffer that can hold a PMCImpl + container
+    char *buff = new char[sizeof(PMCImpl)+sizeof(PMCImpl::Container<ValueType>)];
+
+    //create a new impl object at the start of this buffer
+    PMC p; p.reset(new (buff) PMCImpl());
+
+    //store a pointer to this buffer so it can clean up later
+    p->buff = buff;
+
+    //create a container in the came buffer + offset
+    p->item = new (buff + sizeof(PMCImpl)) PMCImpl::Container<ValueType>(value);
+
     return p;
 }
 
