0229fd0edb0381ca335745509eb5827ce04aa4d7
Josh Blum <josh@joshknows.com>
2012-12-02 15:08:59 -0800

    give all registered types key names

--- include/PMC/Serialize.hpp
+++ include/PMC/Serialize.hpp
@@ -6,11 +6,8 @@
 #include <boost/serialization/export.hpp>
 #include <PMC/Detail/Serialize.hpp>
 
-#define PMC_SERIALIZE_EXPORT_HELP(type) \
-    BOOST_CLASS_EXPORT_KEY2(type, #type) \
-    BOOST_CLASS_EXPORT_IMPLEMENT(type)
-
-#define PMC_SERIALIZE_EXPORT(type) \
-    PMC_SERIALIZE_EXPORT_HELP(PMCImplContainer< type >)
+#define PMC_SERIALIZE_EXPORT(type, key) \
+    BOOST_CLASS_EXPORT_KEY2(PMCImplContainer< type >, key) \
+    BOOST_CLASS_EXPORT_IMPLEMENT(PMCImplContainer< type >)
 
 #endif /*INCLUDED_PMC_SERIALIZE_HPP*/
--- include/PMC/SerializeTypes.hpp
+++ include/PMC/SerializeTypes.hpp
@@ -9,70 +9,70 @@
 /***********************************************************************
  * bool
  **********************************************************************/
-PMC_SERIALIZE_EXPORT(bool)
+PMC_SERIALIZE_EXPORT(bool, "PMC<bool>")
 
 /***********************************************************************
  * integer types
  **********************************************************************/
 #include <boost/cstdint.hpp>
 
-PMC_SERIALIZE_EXPORT(boost::int8_t)
-PMC_SERIALIZE_EXPORT(boost::int16_t)
-PMC_SERIALIZE_EXPORT(boost::int32_t)
-PMC_SERIALIZE_EXPORT(boost::int64_t)
-PMC_SERIALIZE_EXPORT(boost::uint8_t)
-PMC_SERIALIZE_EXPORT(boost::uint16_t)
-PMC_SERIALIZE_EXPORT(boost::uint32_t)
-PMC_SERIALIZE_EXPORT(boost::uint64_t)
+PMC_SERIALIZE_EXPORT(boost::int8_t, "PMC<int8>")
+PMC_SERIALIZE_EXPORT(boost::int16_t, "PMC<int16>")
+PMC_SERIALIZE_EXPORT(boost::int32_t, "PMC<int32>")
+PMC_SERIALIZE_EXPORT(boost::int64_t, "PMC<int64>")
+PMC_SERIALIZE_EXPORT(boost::uint8_t, "PMC<uint8>")
+PMC_SERIALIZE_EXPORT(boost::uint16_t, "PMC<uint16>")
+PMC_SERIALIZE_EXPORT(boost::uint32_t, "PMC<uint32>")
+PMC_SERIALIZE_EXPORT(boost::uint64_t, "PMC<uint64>")
 
 /***********************************************************************
  * float types
  **********************************************************************/
-PMC_SERIALIZE_EXPORT(float)
-PMC_SERIALIZE_EXPORT(double)
+PMC_SERIALIZE_EXPORT(float, "PMC<float32>")
+PMC_SERIALIZE_EXPORT(double, "PMC<float64>")
 
 /***********************************************************************
  * complex float types
  **********************************************************************/
 #include <boost/serialization/complex.hpp>
 
-PMC_SERIALIZE_EXPORT(std::complex<float>)
-PMC_SERIALIZE_EXPORT(std::complex<double>)
+PMC_SERIALIZE_EXPORT(std::complex<float>, "PMC<complex64>")
+PMC_SERIALIZE_EXPORT(std::complex<double>, "PMC<complex128>")
 
 /***********************************************************************
  * string
  **********************************************************************/
 #include <boost/serialization/string.hpp>
 
-PMC_SERIALIZE_EXPORT(std::string)
+PMC_SERIALIZE_EXPORT(std::string, "PMC<string>")
 
 /***********************************************************************
  * pair
  **********************************************************************/
 #include <boost/serialization/utility.hpp>
 
-PMC_SERIALIZE_EXPORT(PMCPair)
+PMC_SERIALIZE_EXPORT(PMCPair, "PMC<Pair>")
 
 /***********************************************************************
  * dict
  **********************************************************************/
 #include <boost/serialization/map.hpp>
 
-PMC_SERIALIZE_EXPORT(PMCDict)
+PMC_SERIALIZE_EXPORT(PMCDict, "PMC<Dict>")
 
 /***********************************************************************
  * list
  **********************************************************************/
 #include <boost/serialization/vector.hpp>
 
-PMC_SERIALIZE_EXPORT(PMCList)
+PMC_SERIALIZE_EXPORT(PMCList, "PMC<List>")
 
 /***********************************************************************
  * set
  **********************************************************************/
 #include <boost/serialization/set.hpp>
 
-PMC_SERIALIZE_EXPORT(PMCSet)
+PMC_SERIALIZE_EXPORT(PMCSet, "PMC<Set>")
 
 /***********************************************************************
  * tuple
@@ -92,17 +92,17 @@ void PMCTuple<0>::serialize(Archive &ar, const unsigned int)
     //NOP - size 0 tuple
 }
 
-PMC_SERIALIZE_EXPORT(PMCTuple<0>)
-PMC_SERIALIZE_EXPORT(PMCTuple<1>)
-PMC_SERIALIZE_EXPORT(PMCTuple<2>)
-PMC_SERIALIZE_EXPORT(PMCTuple<3>)
-PMC_SERIALIZE_EXPORT(PMCTuple<4>)
-PMC_SERIALIZE_EXPORT(PMCTuple<5>)
-PMC_SERIALIZE_EXPORT(PMCTuple<6>)
-PMC_SERIALIZE_EXPORT(PMCTuple<7>)
-PMC_SERIALIZE_EXPORT(PMCTuple<8>)
-PMC_SERIALIZE_EXPORT(PMCTuple<9>)
-PMC_SERIALIZE_EXPORT(PMCTuple<10>)
-PMC_SERIALIZE_EXPORT(PMCTuple<11>)
+PMC_SERIALIZE_EXPORT(PMCTuple<0>, "PMC<Tuple0>")
+PMC_SERIALIZE_EXPORT(PMCTuple<1>, "PMC<Tuple1>")
+PMC_SERIALIZE_EXPORT(PMCTuple<2>, "PMC<Tuple2>")
+PMC_SERIALIZE_EXPORT(PMCTuple<3>, "PMC<Tuple3>")
+PMC_SERIALIZE_EXPORT(PMCTuple<4>, "PMC<Tuple4>")
+PMC_SERIALIZE_EXPORT(PMCTuple<5>, "PMC<Tuple5>")
+PMC_SERIALIZE_EXPORT(PMCTuple<6>, "PMC<Tuple6>")
+PMC_SERIALIZE_EXPORT(PMCTuple<7>, "PMC<Tuple7>")
+PMC_SERIALIZE_EXPORT(PMCTuple<8>, "PMC<Tuple8>")
+PMC_SERIALIZE_EXPORT(PMCTuple<9>, "PMC<Tuple9>")
+PMC_SERIALIZE_EXPORT(PMCTuple<10>, "PMC<Tuple10>")
+PMC_SERIALIZE_EXPORT(PMCTuple<11>, "PMC<Tuple11>")
 
 #endif /*INCLUDED_PMC_SERIALIZE_TYPES_HPP*/
