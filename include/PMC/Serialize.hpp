2e4f7818dc3f5a4197abd81bbe23ea3aa04b5870
Josh Blum <josh@joshknows.com>
2012-12-02 19:44:34 -0800

    bunch of loopback tests for serialize

--- include/PMC/Serialize.hpp
+++ include/PMC/Serialize.hpp
@@ -7,7 +7,6 @@
 #include <PMC/Detail/Serialize.hpp>
 
 #define PMC_SERIALIZE_EXPORT(type, key) \
-    BOOST_CLASS_EXPORT_KEY2(PMCImplContainer< type >, key) \
-    BOOST_CLASS_EXPORT_IMPLEMENT(PMCImplContainer< type >)
+    BOOST_CLASS_EXPORT_GUID(PMCImplContainer< type >, key)
 
 #endif /*INCLUDED_PMC_SERIALIZE_HPP*/
--- tests/serialize_test.cpp
+++ tests/serialize_test.cpp
@@ -1,6 +1,7 @@
 // Copyright (C) by Josh Blum. See LICENSE.txt for licensing information.
 
 #include <boost/test/unit_test.hpp>
+#include <algorithm>
 #include <iostream>
 
 #include <PMC/PMC.hpp>
@@ -11,46 +12,86 @@
 #include <boost/archive/text_iarchive.hpp>
 #include <sstream>
 
-
-//BOOST_CLASS_EXPORT_GUID(PMCImplContainer<int>, "PMCImplContainer<int>")
-
-//BOOST_CLASS_EXPORT_KEY2(PMCImplContainer<int>, "PMCImplContainer<int>")
-//BOOST_CLASS_EXPORT_IMPLEMENT(PMCImplContainer<int>)
-
-BOOST_AUTO_TEST_CASE(test_null)
+static void loopback_test(PMCC p0)
 {
+    std::cout << "doing loopback test on " << p0 << std::endl;
     std::stringstream ss;
     boost::archive::text_oarchive oa(ss);
 
-    PMCC p0;
     oa << p0;
+    std::cout << "stringstream holds " << ss.str() << std::endl;
 
     boost::archive::text_iarchive ia(ss);
     PMCC p1;
     ia >> p1;
 
-    BOOST_CHECK(not p0);
-    BOOST_CHECK(not p1);
+    BOOST_CHECK(p0.eq(p1));
 }
 
-BOOST_AUTO_TEST_CASE(test_foo)
+BOOST_AUTO_TEST_CASE(test_simple_loopbacks)
 {
+    loopback_test(PMC()); //null
+    loopback_test(PMC_M(true));
+    loopback_test(PMC_M(false));
+    loopback_test(PMC_M(int(1234)));
+    loopback_test(PMC_M(long(6789)));
+    loopback_test(PMC_M("hello"));
+}
+
+bool myeq(const PMCC &i, const PMCC &j)
+{
+    return i.eq(j);
+}
+
+template <typename T>
+static void loopback_test_container(const T &t0)
+{
+    PMCC p0 = PMC_M(t0);
+
+    std::cout << "doing loopback test on " << p0 << std::endl;
     std::stringstream ss;
     boost::archive::text_oarchive oa(ss);
 
-    //PMCC p0;
-    //oa << p0;
-
-    PMCC p = PMC_M(int(1234));
-
-    oa << p;
-
-    std::cout << "ss " << ss.str() << std::endl;
+    oa << p0;
+    std::cout << "stringstream holds " << ss.str() << std::endl;
 
     boost::archive::text_iarchive ia(ss);
     PMCC p1;
     ia >> p1;
 
-    std::cout << "p1 " << p1 << std::endl;
-    std::cout << "p1 " << p1.as<int>() << std::endl;
+    BOOST_CHECK(std::equal(t0.begin(), t0.end(), p1.as<T>().begin(), myeq));
+}
+
+BOOST_AUTO_TEST_CASE(test_pmc_tuple_loopback)
+{
+    PMCTuple<0> t0;
+    loopback_test_container(t0);
+
+    PMCTuple<1> t1;
+    t1[0] = PMC_M(42);
+    loopback_test_container(t1);
+
+    PMCTuple<4> t4;
+    t4[0] = PMC_M(5);
+    t4[1] = PMC_M(6);
+    t4[2] = PMC_M(7);
+    t4[3] = PMC_M(8);
+    loopback_test_container(t4);
+}
+
+BOOST_AUTO_TEST_CASE(test_pmc_list_loopback)
+{
+    PMCList l0;
+    loopback_test_container(l0);
+
+    PMCList l1(1);
+    l1[0] = PMC_M(42);
+    loopback_test_container(l1);
+
+    PMCList l4(4);
+    l4[0] = PMC_M(5);
+    l4[1] = PMC_M(6);
+    l4[2] = PMC_M(7);
+    l4[3] = PMC_M(8);
+    loopback_test_container(l4);
 }
