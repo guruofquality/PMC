963f85f9a8e050a6999b9bf4ed600393c5e42011
Josh Blum <josh@joshknows.com>
2013-04-12 01:05:22 -0700

    PMC: added defines to control serilize exports

--- include/PMC/Serialize.hpp
+++ include/PMC/Serialize.hpp
@@ -6,7 +6,21 @@
 #include <boost/serialization/export.hpp>
 #include <PMC/Detail/Serialize.hpp>
 
-#define PMC_SERIALIZE_EXPORT(type, key) \
+#if defined(PMC_SERIALIZE_DECLARE)
+
+    #define PMC_SERIALIZE_EXPORT(type, key) \
+    BOOST_CLASS_EXPORT_KEY2(PMCImplContainer< type >, key)
+
+#elif defined(PMC_SERIALIZE_IMPLEMENT)
+
+    #define PMC_SERIALIZE_EXPORT(type, key) \
+    BOOST_CLASS_EXPORT_IMPLEMENT(PMCImplContainer< type >)
+
+#else
+
+    #define PMC_SERIALIZE_EXPORT(type, key) \
     BOOST_CLASS_EXPORT_GUID(PMCImplContainer< type >, key)
+
+#endif
 
 #endif /*INCLUDED_PMC_SERIALIZE_HPP*/
