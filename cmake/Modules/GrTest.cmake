0a0c1b62daa6340a9b634fa62fec6c2efbcfbe6b
Josh Blum <josh@joshknows.com>
2013-07-31 19:39:46 -0700

    PMC: fix to GrText for maintaining win paths

--- cmake/Modules/GrTest.cmake
+++ cmake/Modules/GrTest.cmake
@@ -67,7 +67,7 @@ function(GR_ADD_TEST test_name)
     file(TO_NATIVE_PATH "${GR_TEST_PYTHON_DIRS}" pypath) #ok to use on dir list?
 
     set(environs "GR_DONT_LOAD_PREFS=1" "srcdir=${srcdir}")
-    list(APPEND environs ${GR_TEST_ENVIRONS})
+    list(APPEND environs "${GR_TEST_ENVIRONS}")
 
     #http://www.cmake.org/pipermail/cmake/2009-May/029464.html
     #Replaced this add test + set environs code with the shell script generation.
